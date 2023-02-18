1afd080d104d02585cf215c43021cd594e47b8a7
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-02-17 18:30:44 -0800

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints all possible combinations of single-digit
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
