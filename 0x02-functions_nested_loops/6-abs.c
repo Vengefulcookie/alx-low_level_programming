4ef1abd1f0c74e0e5bd4b110b3f29c44e12889e7
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-02-22 13:26:16 -0800

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function computes the absolute value of an integer
+ *
+ * @r: to be checked
+ * Return: always r
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+		r = -('r');
+	else if (r >= 0)
+		r = ('r');
+	return (r);
+}
