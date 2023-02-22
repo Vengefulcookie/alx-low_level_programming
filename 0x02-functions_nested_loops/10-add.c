34184eb55097cddcf4742f71d6211a3686f6ebbc
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-02-22 15:13:16 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function adds two integers
+ *
+ * @a: to be checked
+ * @b: to be checked
+ * Return: sum of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - function prints the 9 times table
+ *
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+
