c973328412113d906f673649d7aaa434bafeb40f
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-02-27 12:41:48 -0800

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes pointer to int as parameter
+ * and updates value to 98
+ *
+ * @n: to be checked
+ *
+ * Return: to n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
