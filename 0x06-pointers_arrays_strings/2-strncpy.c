82fd447cfbc75c6d6c2b94ba1aef3e039fbf6364
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-03-02 23:14:04 -0800

    Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function that copies a string
+ *
+ * @src: to be checked
+ * @dest: to be checked
+ * @n: to be checked
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		j++;
+	}
+	return (dest);
+}
