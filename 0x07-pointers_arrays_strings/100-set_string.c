7fe4b14f26539a4119662aa0cc83f8884d73a03e
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-03-06 18:48:04 -0800

    Set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - function sets the value of a pointer to char
+ *
+ * @s: to be checked
+ * @to: pointer char
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
