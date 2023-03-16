26d10743601f0ae57f9eb30729b0f350cb3db425
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-03-16 13:49:40 -0700

    String nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *string_nconcat - concatenates two strings
@@ -17,33 +18,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1 = 0;
 	unsigned int size2 = 0;
 	unsigned int i;
+	unsigned int j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[size1] != '\0')
-	{
+	while (s1 && s1[size1])
 		size1++;
-	}
-	while (s2[size2] != '\0')
-	{
+	while (s2 && s2[size2])
 		size2++;
-	}
 
-	if (n > size2)
-	n = size2;
-	m = malloc((size1 + n + 1) * sizeof(char));
+	if (n < size2)
+		m = malloc(sizeof(char) * (size1 + n + 1));
+	else
+		m = malloc(sizeof(char) * (size1 + size2 + 1));
 
-	if (m == NULL)
-		return (0);
-	for (i = 0; i < size1; i++)
+	if (!m)
+		return (NULL);
+	while (i < size1)
 	{
 		m[i] = s1[i];
+		i++;
 	}
-	for (; i < (size1 + n); i++)
+	while (n > size2 && i < (size1 + size2))
 	{
-		m[i] = s2[i];
+		m[i++] = s2[j++];
 	}
 	m[i] = '\0';
 	return (m);
