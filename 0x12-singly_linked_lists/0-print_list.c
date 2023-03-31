67413172f77491d058008fcbcb31879828fc8ec4
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-03-31 15:13:02 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,13 +5,13 @@
  * print_list - prints all the elements of a list_t list
  *
  * @h: to be checked
- * @m: to be checked
+ * @s: to be checked
  * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t m = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
 	else
 		printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
-			m++;
+			s++;
 	}
 
-	return (m);
+	return (s);
 }
