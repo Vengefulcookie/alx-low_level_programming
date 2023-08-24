99fff9fc8a127d442bec188868011317dcf897bf
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-08-24 15:23:43 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,24 @@
-ll
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len - function that returns the
+ * number of elements in a linked list_t list
+ *
+ * @h: pointer
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t numelem = 0;
+
+	while (h)
+	{
+		numelem++;
+		h = h->next;
+	}
+
+	return (numelem);
+}
