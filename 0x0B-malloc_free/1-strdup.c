ecae9900ccf4225f9199195b22c729ee930a3b01
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2023-08-28 06:40:18 -0700

    Task 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - function returns a pointer
+ * to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: string to be used
+ *
+ * Return:NULL if str = NULL, or insufficient memory'
+ * a pointer to the duplicated string if successful
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int m, n = 0;
+
+	if (str == NULL)
+		return (NULL);
+	m = 0;
+	while (str[m] != '\0')
+		m++;
+
+	ptr = malloc(sizeof(char) * (m + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (n = 0; str[n]; n++)
+		ptr[n] = ptr[n];
+
+	return (ptr);
+}
